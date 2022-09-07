a580b0bd12368b1990d7c68eeb73dec652d066a0
GIOVANI MARQUES <102267202+DevGiovaniMarques@users.noreply.github.com>
2022-09-07 20:04:25 -0300

    criando e validando a conexão do servidor, e as rotas

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 import express, { Request, Response} from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
+import apiRoutes from './routes/api';
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const server = express();
 
 server.use(express.static(path.join(__dirname, '../public')));
 server.use(express.urlencoded({extended:true}));
+
+server.use(apiRoutes);
 
 server.use((req: Request, res: Response) =>{
   res.status(404);
