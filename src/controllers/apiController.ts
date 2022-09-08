b9b8b093308f753f24b35de09b33b217ecf1feef
GIOVANI MARQUES <102267202+DevGiovaniMarques@users.noreply.github.com>
2022-09-08 00:34:45 -0300

    Subindo a versão final - insert,update,delete, select

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { Sequelize } from 'sequelize';
 import { Phrase } from '../models/Phrases';
 
 export const ping = (req: Request, res: Response) => {
@@ -21,3 +22,55 @@ export const createPhrase = async (req: Request, res: Response) => {
   let newPhrase = await Phrase.create({author,phrase});
   res.json({author, phrase});
 }
+
+export const listPhrase = async (req: Request, res: Response) => {
+  let list = await Phrase.findAll();
+  res.json(list);
+}
+
+export const getPhrase = async (req: Request, res: Response) => {
+  let { id } = req.params;
+  let phrase = await Phrase.findByPk(id);
+
+  if(phrase) {
+    res.json(phrase);
+  } else {
+   res.json({error: 'Frase não encontrada'});
+  }
+};
+
+export const updatePhrase = async (req: Request, res: Response) =>{
+  let { id } = req.params;
+  let { author, phrase } = req.body;
+
+  let updatePhrases = await Phrase.findByPk(id);
+
+  if(updatePhrases) {
+    updatePhrases.author = author;
+    updatePhrases.phrase = phrase;
+    await updatePhrases.save();
+    res.json(updatePhrases);
+  } else {
+   res.json({error: 'Frase não encontrada'});
+  }
+};
+
+export const deletePhrase = async (req: Request, res: Response) => {
+  let { id } = req.params;
+
+  await Phrase.destroy({where:{id}});
+  res.json();
+};
+
+export const randomPhrase = async (req: Request, res: Response) => {
+  let phrase = await Phrase.findOne({
+    order:[
+        Sequelize.fn('RANDOM')
+       ]
+  });
+  if(phrase){
+      res.json({phrase});
+    } else {
+      res.json({error: 'Frase não encontrada'});
+    }
+};
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -8,5 +8,10 @@ router.get('/random',ApiController.random );
 router.get('/nome/:nome', ApiController.nome);
 
 router.post('/frases', ApiController.createPhrase);
+router.get('/frases', ApiController.listPhrase);
+router.get('/frases/aleatoria', ApiController.randomPhrase);
+router.get('/frases/:id', ApiController.getPhrase);
+router.put('/frases/:id', ApiController.updatePhrase);
+router.delete('/frases/:id', ApiController.deletePhrase);
 
 export default router;
